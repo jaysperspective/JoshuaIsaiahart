21ee5c6ad33fad53a2ad6daa8e42c1d43629e18f
uranus <83143386+jaysperspective@users.noreply.github.com>
2026-01-26 20:17:27 -0500

    Update route.ts

--- app/api/galleries/[id]/images/route.ts
+++ app/api/galleries/[id]/images/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/app/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { writeFile, mkdir } from "fs/promises";
 import path from "path";
 
